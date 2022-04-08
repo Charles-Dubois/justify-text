3b3c8cfc1f359c61e0f5229bd1f0029c137a06aa
Charles Dubois <charles.dubois.h@gmail.com>
2022-04-08 20:15:32 +0200

    server initialized

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("hello wolrd");
+});
+
+app.listen(PORT, () => console.log("listen"));
+
+app.get("*", (_req, res) => {
+  return res.status(404).json({ message: "error 404 not found" });
+});
